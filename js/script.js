7338b5ce7baa4ad3d7373a5ddf6bf32297327425
SamueleLettieri <samuele2073@gmail.com>
2022-06-28 19:06:48 +0200

    informazioni stampate sul DOM, ma non in colonna

--- js/script.js
+++ js/script.js
@@ -1,39 +1,42 @@
+const element = document.getElementById("element")
+
+
 const team = [
     {
-        nome : "Wayne Barnett",
-        ruolo : "Founder & CEO",
-        foto : "wayne-barnett-founder-ceo.jpg",
+        nome : "Wayne Barnett ",
+        ruolo : "Founder & CEO ",
+        foto : "wayne-barnett-founder-ceo.jpg ",
     },
     {
-        nome : "Angela Caroll",
-        ruolo : "Chief Editor",
-        foto : "angela-caroll-chief-editor.jpg",
+        nome : "Angela Caroll ",
+        ruolo : "Chief Editor ",
+        foto : "angela-caroll-chief-editor.jpg ",
     },
     {
-        nome : "Walter Gordon",
-        ruolo : "Office Manager",
-        foto : "alter-gordon-office-manager.jpg",
+        nome : "Walter Gordon ",
+        ruolo : "Office Manager ",
+        foto : "alter-gordon-office-manager.jpg ",
     },
     {
-        nome : "Angela Lopez",
-        ruolo : "Social Media Manager",
-        foto : "angela-lopez-social-media-manager.jpg",
+        nome : "Angela Lopez ",
+        ruolo : "Social Media Manager ",
+        foto : "angela-lopez-social-media-manager.jpg ",
     },
     {
-        nome : "Scott Estrada",
-        ruolo : "Developer",
-        foto : "wayne-barnett-founder-ceo.jpg",
+        nome : "Scott Estrada ",
+        ruolo : "Developer ",
+        foto : "wayne-barnett-founder-ceo.jpg ",
     },
     {
-        nome : "Barbara Ramos",
-        ruolo : "Graphic Designer",
-        foto : "barbara-ramos-graphic-designer.jpg",
+        nome : "Barbara Ramos ",
+        ruolo : "Graphic Designer ",
+        foto : "barbara-ramos-graphic-designer.jpg ",
     },
 ]
 
 
 for (let i = 0; i < team.length; i++){
-    console.log(team[i].nome);
-    console.log(team[i].ruolo);
-    console.log(team[i].foto);
+    element.innerHTML += team[i].nome;
+    element.innerHTML += team[i].ruolo;
+    element.innerHTML += team[i].foto;
 } 
